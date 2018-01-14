dc8621299fcf9cffdcddcd6a6148811d7a4e2ee2
Kanak Das <1405043.kd@ugrad.cse.buet.ac.bd>
2018-01-14 11:51:19 +0600

    may whitenoise be fixed :(

--- disCountHubF/settings/production.py
+++ disCountHubF/settings/production.py
@@ -128,10 +128,10 @@ USE_TZ = True
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = 'static'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'discounts/static'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
