db134724bfbe05b331727b78684cabda48e6ff37
Kanak Das <1405043.kd@ugrad.cse.buet.ac.bd>
2018-01-14 11:21:39 +0600

    database_fix

--- disCountHubF/settings/production.py
+++ disCountHubF/settings/production.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
 ]
 
@@ -133,4 +134,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
