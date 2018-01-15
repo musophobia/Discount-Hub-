73bda943a3ba6b61438f9370b47bd75a4164ebc9
Kanak Das <1405043.kd@ugrad.cse.buet.ac.bd>
2018-01-16 02:01:01 +0600

    coupon change

--- disCountHubF/settings/production.py
+++ disCountHubF/settings/production.py
@@ -127,11 +127,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(os.path.join(BASE_DIR, 'staticfiles'))
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'discounts/static'),
 )
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
